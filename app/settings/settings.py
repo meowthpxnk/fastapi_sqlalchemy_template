38da30e04c3fc9c45d48a93936e79f7843015bc9
MeowthPxnk <meowthpxnk@gmail.com>
2024-06-09 12:45:37 +0300

    initial

--- app/settings/settings.py
+++ app/settings/settings.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 from dotenv import load_dotenv
 from pydantic import BaseModel, Field
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings
 
 
 load_dotenv()
