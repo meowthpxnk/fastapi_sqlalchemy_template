e37e06dc59bfdbe8a201ecacfc7e7301499739ee
MeowthPxnk <meowthpxnk@gmail.com>
2024-11-01 12:38:18 +0300

    removing base routes

--- app/api/routes/task/__init__.py
+++
@@ -1,4 +0,0 @@
-from app import api
-from .task import tasks_router
-
-api.include_router(tasks_router)
--- app/api/routes/task/task.py
+++
@@ -1,9 +0,0 @@
-from app.api.routes._base_router import APIRouter
-
-
-tasks_router = APIRouter(tags=["task"], prefix="/task")
-
-
-@tasks_router.get("")
-def get_task():
-    return {"ok": True}
